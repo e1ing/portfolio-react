b4bd58a25adcb95cb11f0da1823f6b1fc866495f
e1ing <elvira.kisling@gmail.com>
2021-10-25 08:50:45 +0300

    Skills description and sass skill added.

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -8,6 +8,7 @@ import cssIcon from "../assets/image/css_icon_bk.png";
 import reactIcon from "../assets/image/react_icon_bk.png";
 import reduxIcon from "../assets/image/redux_icon_bk.png";
 import axiosIcon from "../assets/image/axios_icon.png";
+import sassIcon from "../assets/image/sass_icon_bk.png";
 
 function Skills() {
     return (
@@ -17,10 +18,11 @@ function Skills() {
                 <div className={style.skills}>
                     <Skill title={"JavaScript"} styleIcon={{backgroundImage: `url(${jsIcon})`}} description="JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much ..."/>
                     <Skill title={"React"} styleIcon={{backgroundImage: `url(${reactIcon})`}} description={"React is the entry point to the React library. If you load React from a tag, these top-level APIs are available on the React global."}/>
-                    <Skill title={"Redux"} styleIcon={{backgroundImage: `url(${reduxIcon})`}} description={"React is the entry point to the React library. If you load React from a tag, these top-level APIs are available on the React global."}/>
-                    <Skill title={"Axios"} styleIcon={{backgroundImage: `url(${axiosIcon})`}} description={"React is the entry point to the React library. If you load React from a tag, these top-level APIs are available on the React global."}/>
+                    <Skill title={"Redux"} styleIcon={{backgroundImage: `url(${reduxIcon})`}} description={"Redux is open-source JavaScript library for managing and centralizing application state. It is most commonly used with libraries such as React or Angular for building user interfaces."}/>
+                    <Skill title={"Axios"} styleIcon={{backgroundImage: `url(${axiosIcon})`}} description={"is a Javascript library used to make HTTP requests from node.js or XMLHttpRequests from the browser that also supports the ES6 Promise API."}/>
                     <Skill title={"CSS"} styleIcon={{backgroundImage: `url(${cssIcon})`}} description={"CSS is a rule-based language — you define rules specifying groups of styles that should be applied to particular elements or groups ..."}/>
-                    <Skill title={"Git"} styleIcon={{backgroundImage: `url(${gitIcon})`}} description={"React is the entry point to the React library. If you load React from a tag, these top-level APIs are available on the React global."}/>
+                    <Skill title={"Sass"} styleIcon={{backgroundImage: `url(${sassIcon})`}} description={"Sass is the most mature, stable, and powerful professional grade CSS extension language in the world."}/>
+                    <Skill title={"Git"} styleIcon={{backgroundImage: `url(${gitIcon})`}} description={"Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.\n"}/>
                 </div>
             </div>
         </div>
