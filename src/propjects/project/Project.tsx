33058c39e75d39553d11c32b7c9234432593ac4d
Elya <elvira.kisling@gmail.com>
2022-03-18 12:47:22 +0300

    Project.module.scss refactor_2

--- src/propjects/project/Project.tsx
+++ src/propjects/project/Project.tsx
@@ -1,7 +1,5 @@
 import React, {FC} from 'react';
 import style from './Project.module.scss'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faCrosshairs } from '@fortawesome/free-solid-svg-icons';
 
 type ProjectType = {
     projectTitle: string,
@@ -13,15 +11,16 @@ type ProjectType = {
 export const Project: FC<ProjectType> = ({styleImg, projectTitle, projectDescription, linkUrl}) => {
     return (
 
-        <div className={style.project}>
-            <div className={style.overlayStyle}> <img className={style.imageStyle} src={styleImg} alt={"image"}/></div>
-            <div>
-                <div className={style.link}>
-                    <a className={style.a} href={linkUrl}> {/* on github link*/}
-                        <h5 className={style.heading}>Details</h5>
-                    </a>
-                </div>
+        <div className={style.wrapper}>
+            <div className={style.boxImg}>
+                <img className={style.imageStyle} src={styleImg} alt={"image"}/>
+               <div className={style.container}>
+                <a className={style.link} href={linkUrl}> {/* on github link*/}
+                    <h5 className={style.heading}>Details</h5>
+                </a>
+               </div>
             </div>
+
         </div>
 
     )
